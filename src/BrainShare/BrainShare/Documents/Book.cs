0575c6eecce591589a1dd43c274c75bf88249dc2
Alex Shkor <alex.shkor@paralect.com>
2013-04-27 15:15:17 +0300

    changes

---
+++ src/BrainShare/BrainShare/Documents/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BrainShare.Documents
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+
+        public string Title { get; set; }
+
+        public List<string> Owners { get; set; }
+    }
+}
