4aa7198e7608f89b1999f5f7c9304cdc21286de8
EgorTsaryk <egortsaryk@gmail.ru>
2013-04-27 14:47:53 +0400

    bn

---
+++ src/BrainShare/BrainShare/Authentication/CustomAuthentication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrainShare.Documents;
+using BrainShare.MongoDB;
+
+namespace BrainShare.Authentication
+{
+    public class CustomAuthentication : IAuthentication
+    {
+        public CustomAuthentication(MongoDocumentsDatabase database)
+        {
+            this.Database = database;
+        }
+
+        private const string cookieName = "__AUTH_COOKIE";
+        
+        public HttpContext HttpContext { get; set; }
+
+        public MongoDocumentsDatabase Database { get; set; }
+
+        public User Login(string userName, string password, bool isPersistent)
+        {
+            
+        }
+    }
+}
