db086f24f0250834fad9410ead1ebd0247d3f8bb
Alex Shkor <alex.shkor@paralect.com>
2013-04-27 17:46:11 +0300

    search types

--- src/BrainShare/BrainShare/Scripts/custom/addbook.js
+++ src/BrainShare/BrainShare/Scripts/custom/addbook.js
@@ -2,20 +2,31 @@
 var AddBookModel = function() {
     var self = this;
 
-    this.postfix = "&key=AIzaSyAFFukdkjHMHh5WmucwuxVGt18XA9LEJ1I&country=";
+    this.postfix = "&key=AIzaSyAFFukdkjHMHh5WmucwuxVGt18XA9LEJ1I&language=ru&country=";
     this.baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 
     this.query = ko.observable();
     this.selectedLanguage = ko.observable();
+    this.searchType = ko.observable();
 
     this.availableLanguages = ko.observableArray(
        [ { title: "Русский", code: "RU" },
         { title: "Анлглийский", code: "US" }]
     );
+    this.searchTypes = ko.observableArray(
+       [ 
+           { title: "Везде", code: "" },
+           { title: "В названии", code: "+intitle" },
+           { title: "В авторах", code: "+inauthor" },
+           { title: "В издателе", code: "+inpublisher" },
+           { title: "В категориях", code: "+subject" },
+           { title: "ISBN", code: "+isbn" }
+       ]
+    );
     this.items = ko.observableArray();
 
     this.search = function() {
-        $.get(self.baseUrl + self.query() + self.postfix + self.selectedLanguage(),
+        $.get(self.baseUrl + encodeURIComponent(self.query() + self.searchType()) + self.postfix + self.selectedLanguage(),
 
             function (response) {
 
