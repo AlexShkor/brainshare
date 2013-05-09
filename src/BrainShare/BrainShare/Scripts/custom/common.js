7bd8dea16ad4d25117bf41f2c2e8b563110c0fae
Alex Shkor <alex.shkor@gmail.com>
2013-05-09 04:09:05 -0700

    account controller and views deleted, common js expanded

--- src/BrainShare/BrainShare/Scripts/custom/common.js
+++ src/BrainShare/BrainShare/Scripts/custom/common.js
@@ -4,7 +4,7 @@
         type: 'POST',
         traditional: true,
         data: ko.toJS(model),
-        success: function (response) {
+        success: function(response) {
             if (response.Error) {
                 alert(response.Error);
             } else {
@@ -12,4 +12,43 @@
             }
         }
     });
-}
+};
+
+var sendModel = function(url, model, sucessCallback) {
+    send(url, model, function(response) {
+        var defaultBehaviour = true;
+        if (sucessCallback) {
+            var result = sucessCallback(response);
+            if (result === false) {
+                defaultBehaviour = false;
+            }
+        }
+        if (defaultBehaviour) {
+            ko.mapping.fromJS(model, { }, response);
+        }
+    });
+};
+
+var parseValues = function (data, callback, prefix) {
+    prefix = prefix || "";
+    for (var key in data) {
+        if (data[key] == null) {
+            continue;
+        }
+        if (typeof data[key] == "object") {
+            parseValues(data[key], callback, prefix + key);
+        } else {
+            callback(prefix + key, ko.utils.unwrapObservable(data[key]));
+        }
+    }
+};
+
+ko.utils.buildQueryUrl = function (data, excludeKeysList, includeDefaultValues) {
+    var query = "";
+    parseValues(data, function (key, value) {
+        if ((value || includeDefaultValues) && !isKeyExcluded(key, excludeKeysList)) {
+            query += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+        }
+    });
+    return query;
+};
