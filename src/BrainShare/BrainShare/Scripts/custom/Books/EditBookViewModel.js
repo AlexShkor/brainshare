8c721002fb8dcbbb98aea6f3cfe98636a894ae4f
Alex Shkor <alex.shkor@paralect.com>
2013-10-05 16:04:52 +0300

    edit changes

--- src/BrainShare/BrainShare/Scripts/custom/Books/EditBookViewModel.js
+++ src/BrainShare/BrainShare/Scripts/custom/Books/EditBookViewModel.js
@@ -5,4 +5,21 @@
     this.save = function() {
 
     };
+
+    this.addISBN = function() {
+        self.model.ISBNSs.push("");
+    };
+    this.addAuthor = function() {
+        self.model.Authors.push("");
+    };
+    this.removeAuthor = function (author) {
+        if (self.model.Authors().length > 1) {
+            self.model.Authors.remove(author);
+        }
+    };
+    this.removeISBN = function (isbn) {
+        if (self.model.ISBNSs().length > 1) {
+            self.model.ISBNSs.remove(isbn);
+        }
+    };
 }; 
