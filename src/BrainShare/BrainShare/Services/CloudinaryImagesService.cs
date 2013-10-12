e4c1ab4b1ec1067c4b8bfa18739d74ac1f954c1e
Egor Tsaryk <tsaryk_yahor@mail.ru>
2013-10-12 21:36:35 +0300

    missing files

---
+++ src/BrainShare/BrainShare/Services/CloudinaryImagesService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrainShare.Documents;
+using BrainShare.Mongo;
+using MongoDB.Driver;
+
+namespace BrainShare.Services
+{
+    public class CloudinaryImagesService : DocumentsService<CloudinaryImage>
+    {
+        public CloudinaryImagesService(MongoDocumentsDatabase database)
+            : base(database)
+        {
+        }
+
+        protected override MongoCollection<CloudinaryImage> Items
+        {
+            get { return Database.CloudinaryImages; }
+        }
+        
+        public void AddImage(CloudinaryImage image)
+        {
+            Items.Save(image);
+        }
+    }
+}
