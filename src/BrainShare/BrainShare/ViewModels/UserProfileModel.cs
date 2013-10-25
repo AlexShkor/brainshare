92359cafadb4cbdc80f1fcbe81ca25e2966018d8
EgorTsaryk <egortsaryk@gmail.ru>
2013-10-25 03:41:39 +0300

    user profile page

--- src/BrainShare/BrainShare/ViewModels/UserProfileModel.cs
+++ src/BrainShare/BrainShare/ViewModels/UserProfileModel.cs
@@ -9,6 +9,10 @@ namespace BrainShare.Controllers
         public string Id { get; set; }
         public string Name { get; set; }
         public string Avatar { get; set; }
+        public AddressData Address { get; set; }
+        public string Email { get; set; }
+        public string Info { get; set; }
+
         public bool IsMe { get; set; }
         public int SummaryVotes { get; set; }
 
@@ -21,6 +25,9 @@ namespace BrainShare.Controllers
             Name = user.FullName;
             IsMe = user.Id == myId;
             Avatar = user.AvatarUrl ?? Constants.DefaultAvatarUrl;
+            Address = user.Address;
+            Email = user.Email;
+            Info = user.Info;
         }
     }
 }
