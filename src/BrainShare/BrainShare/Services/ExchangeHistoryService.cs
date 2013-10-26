ac7aa3ad648e3f49e7a42b7450bbca92e63a6eb9
Egor Tsaryk <tsaryk_yahor@mail.ru>
2013-10-26 18:58:31 +0300

    missed files, project refactoring

---
+++ src/BrainShare/BrainShare/Services/ExchangeHistoryService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using BrainShare.Documents;
+using BrainShare.Mongo;
+using MongoDB.Driver;
+
+namespace BrainShare.Services
+{
+    public class ExchangeHistoryService: DocumentsService<ExchangeHistory>
+    {
+        public ExchangeHistoryService(MongoDocumentsDatabase database) : base(database)
+        {
+        }
+
+        protected override MongoCollection<ExchangeHistory> Items
+        {
+            get { return Database.ExchangeHistory; }
+        }
+
+        public void SaveExchange(string initiatorId, ExchangeEntry firstUser, ExchangeEntry secondUser)
+        {
+            Save(new ExchangeHistory
+                     {
+                         Id = GenerateId(),
+                         Entries = new List<ExchangeEntry>{firstUser,secondUser},
+                         Initiator = initiatorId,
+                         Present = false
+                     });
+        }
+    }
+}
