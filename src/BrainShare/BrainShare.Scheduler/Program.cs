061b42b4e3234e8820855df0d4379d905f81872a
Anton Putau <anton.parallect@gmail.com>
2013-12-26 16:40:09 +0300

    scheduler changes

--- src/BrainShare/BrainShare.Scheduler/Program.cs
+++ src/BrainShare/BrainShare.Scheduler/Program.cs
@@ -1,36 +1,74 @@
-﻿using System;
-using BrainShare.Scheduler.Jobs;
-using Quartz;
-using Quartz.Impl;
+﻿using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.MessagePatterns;
 
-namespace BrainShare.Scheduler
+namespace BrainShare.Worker
 {
     class Program
     {
+        static readonly string ResponceQueuName = ConfigurationManager.AppSettings["RESPONCE_ISBN_QUEU_NAME"];
+        static readonly string RequestQueuName = ConfigurationManager.AppSettings["REQUEST_ISBN_QUEU_NAME"];
+
+        static readonly ConnectionFactory ConnFactory = new ConnectionFactory
+            {
+                Uri = ConfigurationManager.AppSettings["CLOUDAMQP_URL"],
+                Password = ConfigurationManager.AppSettings["PASSWORD"],
+                UserName = ConfigurationManager.AppSettings["USER"],   
+            };
+
         static void Main(string[] args)
         {
-            //Create the scheduler factory
-            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
- 
-            //Ask the scheduler factory for a scheduler
-            IScheduler scheduler = schedulerFactory.GetScheduler();
- 
-            //Start the scheduler so that it can start executing jobs
-            scheduler.Start();
- 
-            // Create a job of Type WriteToConsoleJob
-            IJobDetail job = JobBuilder.Create(typeof(SetOzBookIsbn)).WithIdentity("MyJob", "MyJobGroup").Build();
- 
-            //Schedule this job to execute every second, a maximum of 10 times
-            ITrigger trigger = TriggerBuilder.Create().WithSchedule(SimpleScheduleBuilder.RepeatSecondlyForTotalCount(10)).StartNow().WithIdentity("MyJobTrigger", "MyJobTriggerGroup").Build();
-            scheduler.ScheduleJob(job, trigger);
- 
-            //Wait for a key press. If we don't wait the program exits and the scheduler gets destroyed
-            Console.ReadKey();
- 
-            //A nice way to stop the scheduler, waiting for jobs that are running to finish
-            scheduler.Shutdown(true);
+            using (var conn = ConnFactory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                // ensure that the queue exists before we access it
+                channel.QueueDeclare(RequestQueuName, false, false, false, null);
+
+                // subscribe to the queue
+                var subscription = new Subscription(channel, RequestQueuName);
+                while (true)
+                {
+                    // this will block until a messages has landed in the queue
+                    var message = subscription.Next();
+
+                    // deserialize the message body
+                    var text = Encoding.UTF8.GetString(message.Body);
+
+                    SendIsbnResponce(text);
+
+                    // ack the message, ie. confirm that we have processed it
+                    // otherwise it will be requeued a bit later
+                    subscription.Ack(message);
+                }
+            }
         }
 
+        private static void SendIsbnResponce(string ozBookId)
+        {
+            Task.Factory.StartNew(() =>
+                {
+                    var isbn = OzParser.OzParser.GetIsbnByOzBookId(ozBookId);
+                    Send(isbn);
+                });
+        }
+
+        private static void Send(string responce)
+        {
+            // Open up a connection and a channel (a connection may have many channels)
+            using (var conn = ConnFactory.CreateConnection())
+            using (var channel = conn.CreateModel()) // Note, don't share channels between threads
+            {
+                // the data put on the queue must be a byte array
+                var data = Encoding.UTF8.GetBytes(responce);
+
+                // ensure that the queue exists before we publish to it
+                channel.QueueDeclare(ResponceQueuName, false, false, false, null);
+
+                // publish to the "default exchange", with the queue name as the routing key
+                channel.BasicPublish("", ResponceQueuName, null, data);
+            }
+        }
     }
 }
